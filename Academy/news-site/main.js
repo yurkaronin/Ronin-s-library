453b1ed956980423c970376848231e649190b270
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2022-01-07 21:27:56 +0300

    Динамические стили + пиксель арт

--- Academy/news-site/main.js
+++ Academy/news-site/main.js
@@ -214,6 +214,55 @@ if (websiteScores) {
   }
 }
 
+// стилизация лонгрида
+let longread = document.querySelector('.longread');
+if (longread) {
+  let colorSetting = document.querySelector('.color-setting');
+  let sizeSetting = document.querySelector('.size-setting');
+  let pixels = document.querySelector('.pixels');
+  let backgroundSetting = document.querySelector('.background-setting');
 
+  colorSetting.onchange = function () {
+    longread.style.color = colorSetting.value;
+  };
+
+  sizeSetting.oninput = function () {
+    pixels.textContent = sizeSetting.value;
+    longread.style.fontSize = sizeSetting.value + 'px';
+  };
+
+  backgroundSetting.onchange = function () {
+    longread.style.backgroundColor = backgroundSetting.value;
+  };
+
+}
+
+// Форма регистрации
+let password = document.querySelector('.password');
+if (password) {
+  let showPassword = document.querySelector('.show-password');
+  let securityBar = document.querySelector('.security-bar');
+
+  showPassword.onchange = function () {
+    if (showPassword.checked) {
+      password.type = 'text';
+    } else {
+      password.type = 'password';
+    }
+  };
+
+  password.oninput = function () {
+    let passLength = password.value.length;
+    securityBar.style.width = passLength * 10 + '%';
+    if (passLength <= 5) {
+      securityBar.style.backgroundColor = 'red';
+    } else if (passLength > 5 && passLength < 10) {
+      securityBar.style.backgroundColor = 'gold';
+    } else {
+      securityBar.style.backgroundColor = 'green';
+    }
+
+  };
+}
 
 
